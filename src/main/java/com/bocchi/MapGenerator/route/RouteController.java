a68f58fb213e8162aa81fa91651b1859cc2a34d5
Moy446 <a22310402@ceti.mx>
2025-05-02 19:19:57 -0600

    cambio de nombre de carpeta

--- src/main/java/com/bocchi/MapGenerator/route/RouteController.java
+++ src/main/java/com/bocchi/MapGenerator/route/RouteController.java
@@ -1,4 +1,4 @@
-package com.bocchi.MapGenerator.Controller;
+package com.bocchi.MapGenerator.route;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
