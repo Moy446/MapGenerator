bfa938c9080a19079eab0df100c19dd0343f6db2
Teisel <lesiet02@gmail.com>
2025-05-08 14:45:04 -0600

    interfaz sin css

--- src/main/java/com/bocchi/MapGenerator/route/RouteController.java
+++ src/main/java/com/bocchi/MapGenerator/route/RouteController.java
@@ -1,14 +1,15 @@
 package com.bocchi.MapGenerator.route;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RouteController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    	return String.format("Hello %s!", name);
+    @GetMapping("/")
+    public String home() {
+    	return "index";
 	}
 
 	@GetMapping("/error")
