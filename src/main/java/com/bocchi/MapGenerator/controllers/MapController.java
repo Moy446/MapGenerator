5e65e3ab2c2c27b148be90b9056bdff52f8716ca
Moy446 <a22310402@ceti.mx>
2025-05-11 22:24:55 -0600

    Mapa procedural con mas colores
    Se mejoro la generacion procedural

--- src/main/java/com/bocchi/MapGenerator/controllers/MapController.java
+++ src/main/java/com/bocchi/MapGenerator/controllers/MapController.java
@@ -4,26 +4,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bocchi.MapGenerator.NoiseGenerator;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
 @RestController
-@RequestMapping("/map")
 public class MapController {
 
-    private final NoiseGenerator noiseGenerator = new NoiseGenerator();
     
     @GetMapping("/getMap")
-    public double[][] getSeed(){
+    public double[][] getMap(@RequestParam(defaultValue = "0") long seed) {
         int width = 100;
         int height = 100;
         double[][] map = new double[width][height];
 
+        NoiseGenerator noiseGenerator = new NoiseGenerator(seed);
+
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 double noise = noiseGenerator.noise(x * 0.1, y * 0.1); // usa una lib de ruido
-                map[x][y] = noise;
+
+                map[x][y] = (noise + 1) / 2.0;
             }
         }
 
