1ec7ce9ba1cd12c6948dabd4d78ad0b3088bf3a2
Teisel <lesiet02@gmail.com>
2025-06-06 00:34:10 -0600

    Dialog para editar el text area

--- src/main/java/com/bocchi/MapGenerator/controllers/MapController.java
+++ src/main/java/com/bocchi/MapGenerator/controllers/MapController.java
@@ -1,21 +1,40 @@
 package com.bocchi.MapGenerator.controllers;
 
+import com.bocchi.MapGenerator.NoiseGenerator;
 import com.bocchi.MapGenerator.parser.MapParseService;
 import com.bocchi.MapGenerator.model.ElementoMapa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/mapa")
 public class MapController {
 
-    @Autowired
-    private MapParseService mapParseService;
+    @GetMapping("/getMap")
+    public double[][] getMap(@RequestParam(defaultValue = "0") long seed) {
+        int width = 100;
+        int height = 100;
+        double[][] map = new double[width][height];
 
-    @PostMapping("/parse")
-    public List<ElementoMapa> parsearMapa(@RequestBody String entrada) throws Exception {
-        return mapParseService.parsearMapa(entrada);
+        NoiseGenerator noiseGenerator = new NoiseGenerator(seed);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                double noise = noiseGenerator.noise(x * 0.1, y * 0.1); // usa una lib de ruido
+
+                map[x][y] = (noise + 1) / 2.0;
+            }
+        }
+
+        return map;
+    }
+
+    //TODO utilizar el lexer para validar el token que mando el usuario
+    @PostMapping("/component")
+    public String component(@RequestBody Map<String, String> body) {
+        String token = body.get("token");
+        return token + "hola";
     }
 }
