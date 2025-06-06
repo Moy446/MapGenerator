0b11db720bc3abbd4da24c19c5194f13c47374b1
Teisel <lesiet02@gmail.com>
2025-06-06 02:31:03 -0600

    Uso de lexer para agregar info

--- src/main/java/com/bocchi/MapGenerator/controllers/MapController.java
+++ src/main/java/com/bocchi/MapGenerator/controllers/MapController.java
@@ -1,11 +1,15 @@
 package com.bocchi.MapGenerator.controllers;
 
 import com.bocchi.MapGenerator.NoiseGenerator;
+import com.bocchi.MapGenerator.parser.Lexer;
 import com.bocchi.MapGenerator.parser.MapParseService;
 import com.bocchi.MapGenerator.model.ElementoMapa;
+import com.bocchi.MapGenerator.parser.sym;
+import java_cup.runtime.Symbol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.StringReader;
 import java.util.List;
 import java.util.Map;
 
@@ -31,10 +35,55 @@ public class MapController {
         return map;
     }
 
-    //TODO utilizar el lexer para validar el token que mando el usuario
     @PostMapping("/component")
-    public String component(@RequestBody Map<String, String> body) {
-        String token = body.get("token");
-        return token + "hola";
+    public String[] component(@RequestBody Map<String, String> body) {
+        String component = body.get("newComponent");
+        String[] valores = {"", "", "", ""};
+        int i = 0;
+
+        try {
+            Lexer lexer = new Lexer(new StringReader(component));
+            Symbol token;
+            int tokenCount = 0; // Contador de tokens
+
+            do {
+                token = lexer.next_token();
+                tokenCount++; // Incrementar contador
+                if(
+                        (tokenCount % 2 != 0) &&
+                        (i < 4)
+                )
+                {
+                       if (i > 0)
+                       {
+                           valores[i] = token.value + "";
+                       }
+                       else
+                       {
+                           valores[i] = token.sym + "";
+                       }
+
+                       i++;
+
+                }
+            } while (token.sym != sym.EOF);
+
+
+        } catch (Exception e) {
+            System.err.println("Error durante el análisis léxico: " + e.getMessage());
+            e.printStackTrace();
+            valores[0] = 404 + "";
+        }
+
+        for (int n = 0; n < 4; n++)
+        {
+            if (valores[n].equals("null") || valores[n].equals(""))
+            {
+                valores[0] = 404 + "";
+            }
+        }
+
+        return valores;
     }
+
 }
